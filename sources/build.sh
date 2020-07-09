c74897db6c9a934b092f8ebc4a02a468fa9c6120
Eli Heuer <elih@protonmail.com>
2020-07-09 14:31:28 -0700

    Update build script to conform to the Google Fonts spec

--- sources/build.sh
+++ sources/build.sh
@@ -4,17 +4,17 @@ set -e
 # It contains all the steps needed to build new fonts,
 # so font builds are repeatable and documented.
 #
-# To build new fonts, open a Unix-like terminal, navigate to
-# the Cairo source root directory, and run the script:
+# To build new fonts, open a Unix-like terminal in the "sources" directory
+# of this project and run the script:
 #
-# $ sh BUILD.sh
+# $ sh build.sh
 #
 # Also, if you are updating the font for Google Fonts, you can
 # use the "-gf" flag to run additional pull-request-helper
 # commands as well. Just remember to change the "pr_dir"
 # file path variable if you aren't building to ~/Google/fonts/ofl/.../:
 #
-# $ sh BUILD.sh -gf
+# $ sh build.sh -gf
 #
 # The default settings should produce output that will conform
 # to the Google Fonts Spec [1] and pass all FontBakery QA Tests [2].
@@ -22,7 +22,7 @@ set -e
 # modified for other platforms and use cases.
 #
 # This script requires Python3 [3] and a Unix-like
-# environment(Mac, Linux, WSL), with a BASH-like shell.
+# environment(Mac, GNU+Linux, WSL), with a BASH-like shell.
 # All Python dependencies will be installed in a temporary
 # virtual environment by the script. Please see the
 # Google Fonts Spec [1] and the FontBakery QA Tools [2] for more info.
@@ -37,42 +37,38 @@ set -e
 #########################
 # BUILD SCRIPT SETTINGS #
 #########################
-alias activate_py_venv=". blue-ocean-build-venv/bin/activate"  # Starts a Python 3 VENV when invoked
+alias activate_py_venv=". build-venv/bin/activate"  # Starts a Python 3 virtual environment when invoked
 output_dir="fonts"                      # Where the output from this script goes
 family_name="Blue Ocean"                # Font family name for output files
 glyphs_source="BlueOcean.glyphs"        # Set the source file to build from
-make_new_venv=true                      # Set to `true` if you want to build and activate a python3 venv as part of the script
-#build_static_fonts=true                 # Set to `true` if you want to build static fonts
-#autohint=false                          # Set to `true` if you want to use auto-hinting (ttfautohint)
+make_new_venv=true                      # Set to `true` if you want to build and activate a python3 virtual environment
+#build_static_fonts=true                # Set to `true` if you want to build static fonts
+#autohint=false                         # Set to `true` if you want to use auto-hinting (ttfautohint)
 nohinting=true                          # Set to `true` if you want the fonts unhinted
 
 ################
 # BUILD SCRIPT #
 ################
-echo "[INFO] Starting build script for $family_name font family"
+echo ""
+echo "[INFO] Starting build script for the $family_name font family"
 
 if [ "$1" = "-gf" ]; then
-  echo "[INFO] And preparing a pull request to Google Fonts";
-fi
-
-if [ -d .git ]; then
-  echo "[TEST] Running from a Git root directory, looks good"
-else
-  echo "[WARN] Font family Git root not found, please run from the root directory"
-  echo "[WARN] Build process cancelled"
-  exit 1
+  echo "[INFO] Also, preparing a pull request to Google Fonts at ~/Google/fonts/ofl";
+  echo ""
 fi
 
 if [ "$make_new_venv" = true ]; then
   echo "[INFO] Building a new Python3 virtual environment"
-  python3 -m venv blue-ocean-build-venv
+  python3 -m venv build-venv > /dev/null
   echo "[INFO] Activating the Python3 virtual environment"
-  activate_py_venv
+  activate_py_venv > /dev/null
   echo "[INFO] Python3 setup..."
-  which python
   pip install --upgrade pip > /dev/null
-  pip install --upgrade -r requirements.txt > /dev/null
+  pip install --upgrade -r ../requirements.txt > /dev/null
+  echo ""
   echo "[INFO] Done with Python3 virtual environment setup"
+  # which python  # Test to see if the VENV setup worked
+  echo ""
 fi
 
 for sources in $glyphs_source; do
@@ -81,24 +77,24 @@ done
 
 for sources in $glyphs_source; do
   echo "[INFO] Building $sources variable fonts with Fontmake..."
-  fontmake -g sources/$sources -o variable \
-    --output-path $output_dir/variable/Blue-Ocean-VF.ttf \
+  fontmake -g $sources -o variable \
+    --output-path ../$output_dir/ttf/BlueOcean.ttf \
     --verbose ERROR
 done
 
 echo "[INFO] Fixing variable font DSIG tables"
-if gftools fix-dsig -f fonts/variable/*-VF.ttf >/dev/null; then
+if gftools fix-dsig -f ../fonts/ttf/*.ttf >/dev/null; then
   echo "[INFO] DSIG fixed for variable fonts"
 else
   echo "[ERROR] GFtools is not working, please update or install: https://github.com/googlefonts/gftools"
 fi
 
 if [ "$nohinting" = true ]; then
-  for font in fonts/variable/*.ttf; do
+  for font in ../fonts/ttf/*.ttf; do
     echo "[INFO] Fixing nonhinting for $font ";
-    gftools fix-nonhinting $font fonts/variable/temp.ttf >/dev/null
-    mv fonts/variable/temp.ttf $font
-    rm -rf fonts/variable/*backup-fonttools-prep-gasp.ttf
+    gftools fix-nonhinting $font ../fonts/ttf/temp.ttf >/dev/null
+    mv ../fonts/ttf/temp.ttf $font
+    rm -rf ../fonts/ttf/*backup-fonttools-prep-gasp.ttf
   done
 fi
 
@@ -111,5 +107,5 @@ fi
 
 if [ "$make_new_venv" = true ]; then
   echo "[INFO] Removing Python3 virtual environment"
-  rm -rf blue-ocean-build-venv
+  rm -rf build-venv
 fi
